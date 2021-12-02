c2d6dbd834a2702d9ccc925f082c64f012908b3e
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2021-12-02 22:23:00 +0100

    Clean up day2

--- 2021/day2.hs
+++ 2021/day2.hs
@@ -1,29 +1,28 @@
 followCourse :: [(String, Int)] -> (Int, Int)
 followCourse moves = go moves (0, 0)
   where
-    go ((d, l) : moves) (x, y)
-      | d == "up" = go moves (x, y - l)
-      | d == "down" = go moves (x, y + l)
-      | d == "forward" = go moves (x + l, y)
+    go ((d, l) : rest) (x, y)
+      | d == "up" = go rest (x, y - l)
+      | d == "down" = go rest (x, y + l)
+      | d == "forward" = go rest (x + l, y)
       | otherwise = undefined
     go _ p = p
 
 followAim :: [(String, Int)] -> (Int, Int)
 followAim moves = go moves (0, 0) 0
   where
-    go ((d, l) : moves) (x, y) aim
-      | d == "up" = go moves (x, y) (aim - l)
-      | d == "down" = go moves (x, y) (aim + l)
-      | d == "forward" = go moves (x + l, y + l * aim) aim
+    go ((d, l) : rest) (x, y) aim
+      | d == "up" = go rest (x, y) (aim - l)
+      | d == "down" = go rest (x, y) (aim + l)
+      | d == "forward" = go rest (x + l, y + l * aim) aim
       | otherwise = undefined
     go _ p _ = p
 
 parseInput :: String -> [(String, Int)]
-parseInput = fmap (mapRight read . pairify . words) . lines
+parseInput = fmap (fmap read . pairify . words) . lines
   where
-    pairify (x : y : xs) = (x, y)
-    pairify _            = undefined
-    mapRight f (x, y) = (x, f y)
+    pairify (x : y : _) = (x, y)
+    pairify _ = undefined
 
 main :: IO ()
 main = do
