8d8d7332dd76038d7017af7922dad789b8575a19
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2021-12-07 00:52:14 +0100

    Messy day

--- 2021/day4.hs
+++ 2021/day4.hs
@@ -1,9 +1,60 @@
-import Text.Parsec
-import Text.Parsec.Combinator
-import Text.Parsec.Token
+import           Data.List
+import           Text.Parsec
+import           Text.Parsec.Combinator
+import           Text.Parsec.Token
 
-solveA numbers boards = go numbers state tests
-  where go x y z = z
+data BoardState = BoardState {
+  getCols     :: [[Int]],
+  getRows     :: [[Int]],
+  getColCount :: [Int],
+  getRowCount :: [Int]
+} deriving (Show)
+
+constructState :: [[Int]] -> BoardState
+constructState board = BoardState {
+  getCols = transpose board,
+  getRows = board,
+  getColCount = 0 <$ board,
+  getRowCount = 0 <$ board
+}
+
+finished :: BoardState -> Bool
+finished state = (any (>= 5) $ getColCount state)
+          || (any (>= 5) $ getRowCount state)
+
+anyFinished :: [BoardState] -> Maybe Int
+anyFinished = findIndex finished
+
+maybeIncrement :: Int -> [Int] -> (Int -> Int)
+maybeIncrement x xs = if x `elem` xs then (+1) else id
+
+applyZip = uncurry ($)
+
+update :: Int -> BoardState -> BoardState
+update x state = BoardState {
+  getCols = getCols state,
+  getRows = getRows state,
+  getColCount = applyZip <$> zip ((maybeIncrement x) <$> (getCols state)) (getColCount state),
+  getRowCount = applyZip <$> zip ((maybeIncrement x) <$> (getRows state)) (getRowCount state)
+}
+
+solveA numbers boards = go numbers (fmap constructState boards) []
+  where
+    go (x:xs) states seen = case anyFinished states of
+      Just y  -> head seen * (sum $ sum <$> (filter (\z -> not (z `elem` seen)) <$> boards !! y))
+      Nothing -> go xs (update x <$> states) (x:seen)
+    go _ _ _ = undefined
+
+solveB numbers boards = go numbers (fmap constructState boards) [] Nothing
+  where
+    go (x:xs) states seen lastFinished = case anyFinished states of
+      Just y ->
+        if length states == 1 then
+          head seen * (sum $ sum <$> (filter (\z -> not (z `elem` seen)) <$> (getCols $ head states)))
+        else
+           go xs (update x <$> (filter (not . finished) states)) (x:seen) (Just (boards !! y))
+      Nothing -> go xs (update x <$> states) (x:seen) lastFinished
+    go _ _ _ _ = undefined
 
 parseBalls = sepBy (read <$> many1 digit) (char ',')
 parseRow = sepBy (read <$> many1 digit) (many1 (char ' '))
@@ -17,10 +68,14 @@ input = do
 
 parseInput :: String -> ([Int], [[[Int]]])
 parseInput it = case parse input "" it of
-    Left _ -> undefined
+    Left _  -> undefined
     Right x -> x
 
 main :: IO ()
 main = do
-  (numbers, boards) <- parseInput <$> readFile "day4a.txt"
-  print $ solveA numbers boards
+  (numbersA, boardsA) <- parseInput <$> readFile "day4a.txt"
+  (numbersB, boardsB) <- parseInput <$> readFile "day4b.txt"
+  print $ solveA numbersA boardsA
+  print $ solveA numbersB boardsB
+  print $ solveB numbersA boardsA
+  print $ solveB numbersB boardsB
