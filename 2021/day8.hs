6b0963fcc889d10192d6095ce579168bc4c64350
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2021-12-08 22:48:46 +0100

    Clean up further

--- 2021/day8.hs
+++ 2021/day8.hs
@@ -1,44 +1,38 @@
-import Data.List
 import Data.Array
+import Data.List
+import Data.Maybe (fromJust)
 import Text.Parsec
 import Text.Parsec.Combinator
 import Text.Parsec.Token
-import Data.Maybe (fromJust)
 
-fromDigits :: [Int] -> Int
-fromDigits = foldl addDigit 0 where addDigit num d = 10*num + d
-
-s = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
-si = fmap (\x -> fromJust $ elemIndex x ['a'..'g']) <$> s
-t mapping = sort . fmap (mapping !!) <$> si
-
--- Mapping from original to scrambled
-testValidity signals mapping = length (filter (`elem` signals) (t mapping)) == length signals
-getMapping signals = fromJust $ find (testValidity signals) (permutations ['a'..'g'])
-
-value :: ([String], [String]) -> Int
-value (signals, output) = fromDigits $ (\x -> fromJust $ elemIndex x (t mapping)) <$> output
-  where mapping = getMapping signals
-
+mapped mapping = sort . fmap (mapping !!) <$> segmentsByIndex
+  where
+    segmentsByIndex =
+      fmap (\x -> fromJust $ elemIndex x ['a' .. 'g'])
+        <$> ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
+test signals mapping = length (filter (`elem` signals) (mapped mapping)) == length signals
+bruteForce signals = fromJust $ find (test signals) (permutations ['a' .. 'g'])
+value (signals, output) = numberify $ (\x -> fromJust $ elemIndex x (mapped mapping)) <$> output
+  where
+    mapping = bruteForce signals
+    numberify = foldl (\x y -> 10 * y + x) 0
 solveB lines = sum $ value <$> lines
 
-parseWord = many1 $ oneOf ['a'..'z']
+countEasy (_, output) = length $ filter (\x -> length x `elem` [2, 3, 4, 7]) output
+solveA lines = sum $ countEasy <$> lines
+
+input = many1 (parseLine <* char '\n')
+parseInput :: String -> [([String], [String])]
+parseInput it = case parse input "" it of
+  Left _ -> undefined
+  Right x -> x
+
+parseWord = many1 $ oneOf ['a' .. 'z']
 parseLine = do
   signals <- count 10 (parseWord <* many (char ' '))
   string "| "
   output <- count 4 (parseWord <* many (char ' '))
   return (sort <$> signals, sort <$> output)
-
-input = many1 (parseLine <* char '\n')
-
-countEasy (_, output) = length $ filter (\x -> length x `elem` [2,3,4,7]) output
-
-solveA lines = sum $ countEasy <$> lines
-
-parseInput :: String -> [([String], [String])]
-parseInput it = case parse input "" it of
-   Left _  -> undefined
-   Right x -> x
 
 main :: IO ()
 main = do
